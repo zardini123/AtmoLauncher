8d2d379675bfed6717be6037bcdf7f0eb1b34421
Pedro Lopez <pslamoros@hotmail.com>
2016-05-17 02:09:17 -0400

    Fixed issues that occured when a file's name had spaces in it, implemented update progress saving.

---
+++ UpdateLib/UpdateProgress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateLib
+{
+    public class UpdateProgress
+    {
+        public List<string> DownloadedFiles;
+
+        public UpdateProgress() {
+            DownloadedFiles = new List<string>();
+        }
+    }
+}
