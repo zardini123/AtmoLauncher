04044cbc271b7dc2eae9a59e0ad70d406f5ab7a0
Taras Palczynski <tarasikpalczynski@gmail.com>
2016-06-12 17:15:06 -0400

    Base for Native MacOS AtmoLauncher

---
+++ Native MacOS Version/AtmoLauncher/AtmoLauncher/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  AtmoLauncher
+//
+//  Created by Tarasik A. Palczynski on 6/12/16.
+//  Copyright © 2016 OneMoreBlock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
+
